01c335099366fe37bed0ef8a5bfe9db38b6a4cf1
Kirill Jeleb <kirhoc0898@gmail.com>
2017-06-15 15:59:45 +0300

    work with two controlers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-/**
-* app Module
-*
-* Description
-*/
-angular.module('app', []).controller('myCtrl',  function($scope){
-	$scope.hello = 55;
-	$scope.myBook = "Boom"
+var app = angular.module('app', []);
+
+app.controller('First',  function($scope , myfectory){
+	console.log("First");
+	$scope.myfectory = myfectory;
+	$scope.hello = "Hello World 1";
+});
+
+app.controller('Second',  function($scope , myfectory){
+	$scope.myfectory = myfectory;
+	console.log("Second");
+	$scope.hello = "Hello World 2";
+});
+
+app.factory('myfectory',  function(){
+	return{
+		hello: 'Hello World'
+	}
 })
