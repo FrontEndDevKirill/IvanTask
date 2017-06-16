03cd8b28046f4c8745ccbd4363fc4183c36d895d
Kirill Jeleb <kirhoc0898@gmail.com>
2017-06-16 18:35:12 +0300

    Search is ready

--- js/main.js
+++ js/main.js
@@ -12,7 +12,17 @@ app.controller('main',  function($scope){
 	$scope.skill = "";
 	$scope.level = "";
 
+	$scope.nameses = ["Simon","Stiv","Goder","Kirill","Den","Carl","Meko","Dancer","Boltan","Goter"];
+	$scope.workses = ["DEV","FronEnd","BackEnd","Sinior","Jun","Midle","Boss","Denes","Tark","vb"];
+	$scope.skillses = ["cs","cpp","java","html","css","js","php","ja","jq","nj"];
+	$scope.leveles = ["1","2","3","4","5","6","7","8","9","10"];
 
+
+
+	$scope.getRandomNum = function(){
+	  return Math.floor((Math.random()*6)+0);
+	}
+	$scope.rand = $scope.getRandomNum();
 
 	$scope.AddSkills = function() {
 		if ($scope.skill == "" || $scope.level == "") {
@@ -48,8 +58,26 @@ app.controller('main',  function($scope){
 	}
 
 	$scope.AddRandomUser = function () {
-		
+		$scope.rand = $scope.getRandomNum();
+		$scope.Users[$scope.Users.length - 1].name = $scope.nameses[$scope.getRandomNum()];
+		$scope.Users[$scope.Users.length - 1].work = $scope.workses[$scope.getRandomNum()];
+		for (var i = 0; i < $scope.rand+1; i++) {
+			$scope.Users[$scope.Users.length - 1].skills.push({
+				title: $scope.skillses[$scope.getRandomNum()],
+				level: $scope.leveles[$scope.getRandomNum()]
+			});
+		}
+
+
+			$scope.Users.push({
+				name:"",
+				work:"",
+				skills:[]
+			});
 	}
+	$scope.removeItem = function (x) {
+        $scope.Users[0].skill.splice(x, 1);
+    }
 
 })
 
