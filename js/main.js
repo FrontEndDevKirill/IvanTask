89675f260d6ce8b1de308a50a612ebdc507bc0d3
Kirill Jeleb <kirhoc0898@gmail.com>
2017-06-16 15:37:10 +0300

    AllReady without random and search

--- js/main.js
+++ js/main.js
@@ -13,9 +13,10 @@ app.controller('main',  function($scope){
 	$scope.level = "";
 
 
+
 	$scope.AddSkills = function() {
 		if ($scope.skill == "" || $scope.level == "") {
-			console.log("Не все поля заполнены");
+			alert("Не все поля заполнены");
 		}
 		else{
 			$scope.Users[$scope.Users.length - 1].skills.push({
@@ -25,6 +26,29 @@ app.controller('main',  function($scope){
 			$scope.skill = "";
 			$scope.level = "";
 		}
+	}
+
+	$scope.AddUser = function(){
+		if ($scope.name == "" || $scope.Prof == "") {
+			alert("Не все поля заполнены");
+		}
+		else{
+			$scope.Users[$scope.Users.length-1].name = $scope.name;
+			$scope.Users[$scope.Users.length-1].work = $scope.Prof;
+			$scope.name = "";
+			$scope.Prof = "";
+			$scope.skill = "";
+			$scope.level = "";
+			$scope.Users.push({
+				name:"",
+				work:"",
+				skills:[]
+			});
+		}
+	}
+
+	$scope.AddRandomUser = function () {
+		
 	}
 
 })
