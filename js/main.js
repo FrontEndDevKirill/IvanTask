b3b1ee19553b5e443ab749bd45e8f20078becda3
Kirill Jeleb <kirhoc0898@gmail.com>
2017-06-16 13:35:59 +0300

    Добавил jq и Добавление скила и левела

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 var app = angular.module('app', []);
 
+app.controller('main',  function($scope){
+	$scope.Users = [
+		{
+			name:"",
+			work:"",
+			skills:[]
+		}
+	];
+	//scope.i = 0;
+	$scope.skill = "";
+	$scope.level = "";
+
+
+	$scope.AddSkills = function() {
+		if ($scope.skill == "" || $scope.level == "") {
+			console.log("Не все поля заполнены");
+		}
+		else{
+			$scope.Users[$scope.Users.length - 1].skills.push({
+				title: $scope.skill,
+				level: $scope.level
+			});
+			$scope.skill = "";
+			$scope.level = "";
+		}
+	}
+
+})
+
