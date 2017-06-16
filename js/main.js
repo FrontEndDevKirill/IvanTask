4bab39d09999d6b9da51627b381a0299c8e7d852
Kirill Jeleb <kirhoc0898@gmail.com>
2017-06-16 09:28:27 +0300

    start todey

--- js/main.js
+++ js/main.js
@@ -1,17 +1,2 @@
 var app = angular.module('app', []);
 
-app.controller('MyBooks', function($scope){
-		$scope.showbook = function(){
-		console.log('ggg');
-	}
-});
-
-app.controller('angularBookControler', function($scope){
-	$scope.showbook = function(){
-		console.log('fff');
-	}
-});
-
-app.controller('emberBookctrl', function($scope){
-	
-});
